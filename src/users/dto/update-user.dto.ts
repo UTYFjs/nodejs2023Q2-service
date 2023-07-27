9f771d59508e47ad6fbeea8142682de6bb5e72ee
UTYFjs <utyf-90@mail.ru>
2023-07-27 13:15:17 +0300

    feat: add dto validation

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -4,6 +4,10 @@ import { IsString, IsNotEmpty } from 'class-validator';
 //export class UpdateUserDto extends PartialType(CreateUserDto) {}
 
 export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string; // previous password
+  @IsString()
+  @IsNotEmpty()
   newPassword: string;
 }
